72b99adf4138816490ca40a97890d8a09dcad41f
arehmandev <arehmandev@outlook.com>
2016-12-05 13:02:53 +0000

    added wait

--- VMbootstrap.sh
+++ VMbootstrap.sh
@@ -2,8 +2,12 @@
 
 yum update -y
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+wait 3
 yum install -y gcc-c++ make git nodejs
+wait 3
 npm install -g polymer-cli
+wait 3
 npm install -g bower
+wait 3
 cd $PWD/shop
 bower install --allow-root
